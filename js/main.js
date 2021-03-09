ff92fae5de9bcce30549a0f1950f1158a0bb0dde
Vladimir <vsim1964@gmail.com>
2021-03-09 21:51:09 +0300

    Исправлен слайдер

--- js/main.js
+++ js/main.js
@@ -1,44 +1,34 @@
-$(function(){
+$(function () {
 
 
-   $('.promo-slider-list').slick({
-      dots: true,
-      prevArrow: '<button class="promo-slider-list-btn promo-slider-list-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
-      nextArrow: '<button class="promo-slider-list-btn promo-slider-list-btnnext"><img src="images/arrow-right.svg" alt=""></button>',
-   });
-
-
-/*  $('.search-tabs-item').on('click', function(e){
-      e.preventDefault();
-
-      $('.search-tabs-item').removeClass('search-tabs-item-active');
-      $('.search-content-item').removeClass('search-content-item-active');
-
-      $(this).addClass('search-tabs-item-active');
-      $($(this).attr('href')).addClass('search-content-item-active')
-});  */
-
-$('.tab').on('click', function(e){
-	e.preventDefault();
-
-	$('.tab').removeClass('tab-active');
-	$('.tab-content').removeClass('tab-content-active');
-
-	$(this).addClass('tab-active');
-	$($(this).attr('href')).addClass('tab-content-active');
-});
-	
-$('.love-add').on('click', function () {
-	$('.love-add').toggleClass('love-add-active');
-
+	$('.promo-slider-list').slick({
+		dots: true,
+		prevArrow: '<button class="promo-slider-list-btn promo-slider-list-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
+		nextArrow: '<button class="promo-slider-list-btn promo-slider-list-btnnext"><img src="images/arrow-right.svg" alt=""></button>'
 	});
 
-$('.popular-content-list').slick({
-		
-	slidesToShow: 2,
-	slidesToScroll: 1,
 
+	$('.tab').on('click', function (e) {
+		e.preventDefault();
 
+		$($(this).siblings()).removeClass('tab-active');
+		$($(this).parent().siblings().find('div')).removeClass('tab-content-active');
+
+		$(this).addClass('tab-active');
+		$($(this).attr('href')).addClass('tab-content-active')
 	});
-	
+
+	$('.love-add').on('click', function () {
+		$('.love-add').toggleClass('love-add-active')
+	});
+
+	$('.popular-content-list').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+ prevArrow: '<button class="popular-slider-list-btn popular-slider-list-btnprev"><img src="images/black-left.svg" alt=""></button>',
+nextArrow: '<button class="popular-slider-list-btn popular-slider-list-btnnext"><img src="images/black-right.svg" alt=""></button>'
+	});
+
+
 });
+/* */
